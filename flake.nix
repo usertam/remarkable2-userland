20feea7b6f2628458e4769e0b01f7691cd23fa96
usertam <code@usertam.dev>
2025-09-24 00:59:30 +0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    {
+      remarkable2Packages = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (
+        system: nixpkgs.legacyPackages.${system}.pkgsCross.remarkable2.pkgsStatic
+      );
+
+      # The package set I actually want to build in CI.
+      packages = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (system: {
+        inherit (self.remarkable2Packages.${system}) curl file;
+      });
+    };
+}
