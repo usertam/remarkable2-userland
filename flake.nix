230b428f798d30bcde982c37fdb380adf699744d
usertam <code@usertam.dev>
2025-10-01 16:16:46 +0800

    formalize packages into groups

--- flake.nix
+++ flake.nix
@@ -7,32 +7,37 @@
     { self, nixpkgs }:
     {
       remarkable2Packages = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (
-        system: nixpkgs.legacyPackages.${system}.pkgsCross.remarkable2.pkgsStatic
+        system: nixpkgs.legacyPackages.${system}.pkgsCross.remarkable2.pkgsStatic.extend (final: prev: {
+          iproute2 = prev.iproute2.override { python3 = null; };
+        })
       );
 
-      packages = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (system: rec {
-        remarkable2-userland = nixpkgs.legacyPackages.${system}.runCommand "remarkable2-userland" {
-          srcs = with self.remarkable2Packages.${system}; [
-            coreutils file findutils util-linux which
-            diffutils gnugrep gnused gnupatch jq less
-            curl inetutils rsync # nmap (liblinear), ndisc6 (perl), dig (bind)
-            btop procps lsof strace
-            gnutar pigz # (pixz.override { asciidoc = null; }) (https://github.com/vasi/pixz/issues/67)
-            nano (tailscale.override { iproute2 = iproute2.override { python3 = null; }; })
-            (iproute2.override { python3 = null; })
-          ];
+      packages = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (system: {
+        remarkable2-userland = nixpkgs.legacyPackages.${system}.runCommand "remarkable2-userland" rec {
+          srcs = nixpkgs.lib.mapAttrsToList (
+            name: value:
+            self.remarkable2Packages.${system}.${name}
+          ) (import ./groups.nix);
           nativeBuildInputs = with nixpkgs.legacyPackages.${system}; [
             gnutar pixz
           ];
-        } ''
+        }
+        (''
           mkdir -p $out/bin
-          find $srcs -type f -executable -exec cp -n {} $out/bin \;
+          install -Dm555 -t $out/bin \
+            ${builtins.concatStringsSep " \\\n  " (nixpkgs.lib.mapAttrsToList (
+              name: value: let
+                wrapIfMany = nixpkgs.lib.optionalString (builtins.length value > 1);
+                bins = wrapIfMany "{" + builtins.concatStringsSep "," value + wrapIfMany "}";
+              in
+                self.remarkable2Packages.${system}.${name} + "/bin/" + bins
+            ) (import ./groups.nix))}
 
           mkdir -p $out/tarball
           cd $out/bin
           time tar --sort=name --mtime='@1' --owner=0 --group=0 --numeric-owner -c * .* | \
             pixz -t > $out/tarball/userland.tar.xz
-        '';
+        '');
       });
     };
 }
---
+++ groups.nix
@@ -0,0 +1,119 @@
+{
+  coreutils = [
+    "cp" "mv" "rm" "ln"
+    "mkdir" "mkfifo" "mknod" "mktemp"
+    "truncate" "touch" "install"
+    "basename" "dirname" "realpath"
+    "cat" "tac" "head" "tail"
+    "split" "cut" "tr" "sort" "uniq" "wc"
+    "tee" "fold" "yes"
+    "chmod" "chown" "chgrp"
+    "md5sum" "sha256sum" "sha512sum"
+    "id" "who" "whoami" "logname"
+    "uname" "uptime" "tty"
+    "nice" "nohup" "timeout"
+    "stdbuf" "chroot"
+    "env" "expr" "factor" "seq"
+    "df" "du" "stat" "sync"
+    "dircolors"
+    "date" "sleep"
+    "coreutils"
+  ];
+
+  file = [ "file" ];
+
+  findutils = [
+    "find" "locate" "updatedb" "xargs"
+  ];
+
+  util-linux = [
+    "fallocate" "fdisk" "losetup" "lsblk"
+    "mount" "umount" "mountpoint"
+    "flock" "ionice" "renice" "nsenter" "unshare"
+    "setarch" "setpriv" "setsid" "setterm"
+    "dmesg" "logger" "last"
+    "rfkill" "lscpu" "hwclock"
+    "hexdump" "rev" "look" "more"
+    "cal" "uuidgen" "getopt" "whereis"
+  ];
+
+  which = [ "which" ];
+
+  diffutils = [
+    "diff" "cmp"
+    "diff3" "sdiff"
+  ];
+
+  gnugrep = [
+    "grep"
+    "egrep" "fgrep"
+  ];
+
+  gnused = [ "sed" ];
+
+  gnupatch = [ "patch" ];
+
+  jq = [ "jq" ];
+
+  less = [ "less" ];
+
+  curl = [ "curl" ];
+
+  inetutils = [
+    "ping" "ping6" "traceroute"
+    "hostname" "logger" "whois"
+  ];
+
+  rsync = [ "rsync" ];
+
+  # nmap = [
+  #   "nmap" "ncat" "nping"
+  # ];
+
+  # ndisc6 = [
+  #   "ndisc6" "rltraceroute6" "tcptraceroute6" "tracert6"
+  #   "tcpspray" "tcpspray6"
+  # ];
+
+  btop = [ "btop" ];
+
+  procps = [
+    "kill" "pgrep" "pkill" "pidof"
+    "ps" "top" "w" "pmap"
+    "free" "vmstat" "uptime" "watch"
+    "sysctl"
+  ];
+
+  lsof = [ "lsof" ];
+
+  strace = [ "strace" ];
+
+  gnutar = [ "tar" ];
+
+  pigz = [ "pigz" "unpigz" ];
+
+  nano = [ "nano" "rnano" ];
+
+  tailscale = [ "tailscale" "tailscaled" ];
+
+  getent = [ "getent" ];
+
+  iproute2 = [
+    "ip" "ss" "tc"
+    "ifstat" "lnstat" "nstat" "rtstat" "ctstat"
+    "rtacct" "rtmon" 
+    "routel"
+  ];
+
+  iptables = [
+    "iptables" "iptables-apply" "iptables-restore" "iptables-restore-translate" "iptables-save" "iptables-translate" "iptables-xml"
+    "iptables-nft" "iptables-nft-restore" "iptables-nft-save" 
+    "ip6tables" "ip6tables-apply" "ip6tables-restore" "ip6tables-restore-translate" "ip6tables-save" "ip6tables-translate"
+    "ip6tables-nft" "ip6tables-nft-restore" "ip6tables-nft-save"
+    "arptables" "arptables-restore" "arptables-save" "arptables-translate"
+    "arptables-nft" "arptables-nft-restore" "arptables-nft-save"
+    "ebtables" "ebtables-restore" "ebtables-save" "ebtables-translate"
+    "ebtables-nft" "ebtables-nft-restore" "ebtables-nft-save"
+    "nfbpf_compile" "nfnl_osf" "nfsynproxy" "xtables-monitor" "xtables-nft-multi"
+  ];
+}
